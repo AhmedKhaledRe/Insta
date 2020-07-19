4926f35739e96603af8c63a7d431ae2e92df05db
Ahmed Khaled <36384512+AhmedKhaledRe@users.noreply.github.com>
2020-07-19 22:01:46 +0200

    first

---
+++ README.md
@@ -0,0 +1,16 @@
+#To Run
+    php artisan serve
+
+
+#End Point Routes
+    profile/1
+    profile/2
+    ....
+
+    p/create
+
+    p/1
+    p/2
+    ....
+
+    
---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Welcome to insta
+
+This is a community of fellow developers and we love that you have joined us.
+
+@component('mail::button', ['url' => ''])
+Button Text
+@endcomponent
+
+All the best,<br>
+Ahmed Khaled
+@endcomponent
